f95cdf856c6e90b78f3c98699c4ff9a0e465183f
McQuade <mcquxde@gmail.com>
2025-12-05 22:28:40 -0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        // 1. The Main Page (Your portfolio or landing page)
+        main: resolve(__dirname, 'index.html'),
+        
+        // 2. Your Sub-Projects (CHANGE 'folder-name' to your actual folder names)
+        projectOne: resolve(__dirname, 'TCG/tcg.html'),
+      },
+    },
+  },
+})
